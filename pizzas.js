2a62cafecad829ed19399b0b9c78144354a4767a
LucasFr <43897881+birnayt@users.noreply.github.com>
2020-06-07 13:50:44 -0400

    Update pizza.js
    Creating of pizzaPrices object to change the value when select some size in the window area.

--- pizzas.js
+++ pizzas.js
@@ -1,11 +1,15 @@
+let pizzaPrices = {
+    promotion: [19.99, 22.99, 24.99]
+}
+
 let pizzaJson = [
-    { id: 1, name: 'Mussarela', img: 'images/pizza.png', price: 20.19, sizes: ['100g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
-    { id: 2, name: 'Calabresa', img: 'images/pizza2.png', price: 18.00, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
-    { id: 3, name: 'Quatro Queijos', img: 'images/pizza3.png', price: 17.45, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
-    { id: 4, name: 'Americana', img: 'images/pizza4.png', price: 19.77, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
-    { id: 5, name: 'Sorvete', img: 'images/pizza5.png', price: 21.43, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
-    { id: 6, name: 'Moda da Casa', img: 'images/pizza6.png', price: 18.55, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
-    { id: 7, name: 'Chocolate', img: 'images/pizza7.png', price: 22.36, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' }
+    { id: 1, name: 'Mussarela', img: 'images/pizza.png', price: pizzaPrices.promotion, sizes: ['100g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+    { id: 2, name: 'Calabresa', img: 'images/pizza2.png', price: pizzaPrices.promotion, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+    { id: 3, name: 'Quatro Queijos', img: 'images/pizza3.png', price: pizzaPrices.promotion, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+    { id: 4, name: 'Americana', img: 'images/pizza4.png', price: pizzaPrices.promotion, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+    { id: 5, name: 'Sorvete', img: 'images/pizza5.png', price: pizzaPrices.promotion, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+    { id: 6, name: 'Moda da Casa', img: 'images/pizza6.png', price: pizzaPrices.promotion, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+    { id: 7, name: 'Chocolate', img: 'images/pizza7.png', price: pizzaPrices.promotion, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' }
 ];
 
 function createPizzas() {
@@ -26,16 +30,27 @@ function createPizzas() {
         const itemImg = document.createElement('img');
         const pizzaItemAdd = document.createElement('div');
 
-
         pizzaItemImg.classList.add('pizza-item--img');
         pizzaItemAdd.classList.add('pizza-item--add');
-        pizzaItemAdd.setAttribute('onclick', `showWindowArea(${pizzaJson[pizza].id - 1})`);
+        pizzaItemAdd.setAttribute('onclick', `showWindowArea(${pizza})`);
 
         itemImg.src = `${pizzaJson[pizza].img}`;
         pizzaItemAdd.innerHTML = '+';
-        pizzaPrice.innerHTML = `R$ ${pizzaJson[pizza].price}`;
-        pizzaName.innerHTML = `${pizzaJson[pizza].name}`;
+        pizzaPrice.innerHTML = `R$ ${pizzaPrices.promotion[2]}`;
+        pizzaName.innerHTML = `${pizzaJson[pizza].name} (G)`;
         pizzaDesc.innerHTML = `R$ ${pizzaJson[pizza].description}`;
+
+
+        pizzaItemImg.appendChild(itemImg);
+        aItem.append(pizzaItemImg, pizzaItemAdd);
+        pizzaItem.append(aItem, pizzaPrice, pizzaName, pizzaDesc);
+
+        document.querySelector('.pizza-area').appendChild(pizzaItem);
+    }
+};
+
+createPizzas();
+
 
 
         pizzaItemImg.appendChild(itemImg);
